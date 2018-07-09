ae2b7e9eddcb4f848490d0c6ebbbccb4700dd3c9
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2018-07-08 23:49:49 -0300

    Recursos do html mais o css e integrado o Js

---
+++ funcaoArrow.js
@@ -0,0 +1,16 @@
+somaValores = (a,b) =>{
+  return a + b;
+}
+somaValores(2,2);
+
+//Função Arrow
+somaValores = a => a + b;
+somaValores(4,4);
+
+function Pessoa(){
+  this.idade = 21;
+
+  return idade;
+}
+
+Pessoa();
