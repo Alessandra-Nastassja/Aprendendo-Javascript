5bf3acac96b8c3c7adf7a3a3a4a9f569ab5640a0
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2018-07-16 23:01:19 -0300

    Funções CallBack, Class e Factory

---
+++ funcaoCallBack.js
@@ -0,0 +1,20 @@
+const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
+//SEM CALLBACK
+let notasBaixas = [];
+for(let i in notas){
+  if(notas[i] < 7){
+    notasBaixas.push(notas[i])/
+  }
+}
+
+console.log(notasBaixas);
+
+//COM CALLBACK
+const notasBaixas2 = notas.filter(function (notas){
+    return notas < 7;
+})
+
+notasBaixas2
+
+const notasBaixas3 = notas.filter(nota => nota < 7)
+notasBaixas3
